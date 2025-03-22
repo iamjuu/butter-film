803fac2fcc367aeff81f00f48f8878fb8eaba077
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2025-03-22 17:57:36 +0530

    bg added

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import Instagram from './icon/Instagram.svg'
+import Banner1 from './image/banner/Rifle-Club-review-The-Week.svg'
+import Banner2 from './image/banner/Pexels Photo by Anna Frolova.svg'
+import Banner3 from './image/banner/Pexels Photo by Suzy Hazelwood.svg'
+export{
+    Instagram,
+    Banner1,
+    Banner2, 
+    Banner3
+}
